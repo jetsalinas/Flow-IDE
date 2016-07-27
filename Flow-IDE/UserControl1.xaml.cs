959f937bb2c96133da3120ecc49860f217fc90ea
jetsalinas <joseenrico.salinas@gmail.com>
2016-07-27 15:25:09 +0800

    Added Tab Panel

--- Flow-IDE/UserControl1.xaml.cs
+++ Flow-IDE/UserControl1.xaml.cs
@@ -12,16 +12,15 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Markup;
 
 namespace Flow_IDE
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class UserControl1 : UserControl
     {
-        public MainWindow()
+        public UserControl1()
         {
             InitializeComponent();
         }
